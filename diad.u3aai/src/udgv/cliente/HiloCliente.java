0e2ae3bd1b4eed68182620881eb329612b6af6c7
Mariano Antonio Soto Hernandez <mantiniok0051@gmail.com>
2018-12-07 11:39:41 -0600

    Correccion
    Correccon del flujo 
    Integracion de mensajes mas descriptivos

--- diad.u3aai/src/udgv/cliente/HiloCliente.java
+++ diad.u3aai/src/udgv/cliente/HiloCliente.java
@@ -12,14 +12,14 @@ import java.net.*;
 import java.io.*;
 
 public class HiloCliente extends Thread {
-	   private Socket    		socket   = null;
-	   private Cliente   		client   = null;
-	   private DataInputStream  streamIn = null;
+	   private Socket    		socket    = null;
+	   private Cliente   		cliente   = null;
+	   private DataInputStream      entrada = null;
 	   
 	   
 
 	   public HiloCliente(Cliente _cliente, Socket _socket)
-	   {  client   = _cliente;
+	   {  cliente   = _cliente;
 	      socket   = _socket;
 	      open();  
 	      start();
@@ -28,29 +28,29 @@ public class HiloCliente extends Thread {
 	  
 	   public void open()
 	   {  try
-	      {  streamIn  = new DataInputStream(socket.getInputStream());
+	      {  entrada  = new DataInputStream(socket.getInputStream());
 	      }
 	      catch(IOException ioe)
-	      {  System.out.println("Error getting input stream: " + ioe);
-	         client.stop();
+	      {  System.out.println("--- Ups! Encontramos un problema: " + ioe);
+	         cliente.stop();
 	      }
 	   }
 	   public void close()
 	   {  try
-	      {  if (streamIn != null) streamIn.close();
+	      {  if (entrada != null) entrada.close();
 	      }
 	      catch(IOException ioe)
-	      {  System.out.println("Error closing input stream: " + ioe);
+	      {  System.out.println("--- Ups! Encontramos un problema: " + ioe);
 	      }
 	   }
 	   public void run()
 	   {  while (true)
 	      {  try
-	         {  client.handle(streamIn.readUTF());
+	         {  cliente.handle(entrada.readUTF());
 	         }
 	         catch(IOException ioe)
-	         {  System.out.println("Listening error: " + ioe.getMessage());
-	            client.stop();
+	         {  System.out.println("--- Diantres! algo sucedio durante la escucha: " + ioe.getMessage());
+	            cliente.stop();
 	         }
 	      }
 	   }
